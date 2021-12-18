42ba5c12b369d0b538579685d86842cdee23d97e
Md. Jamal Uddin <20262255+jaamaal95@users.noreply.github.com>
2021-12-18 14:49:52 +0600

    add project

---
+++ src/App.tsx
@@ -0,0 +1,17 @@
+import "./App.css";
+import { Form } from "./components/Form";
+
+function App() {
+  return (
+    <>
+      <header>
+        <h2>Serverless React + TypeScript!</h2>
+      </header>
+      <main>
+        <Form />
+      </main>
+    </>
+  );
+}
+
+export default App;
